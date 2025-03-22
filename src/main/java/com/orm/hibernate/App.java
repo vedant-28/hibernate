a7cc6d14a017caf2258dd17a91503f6dd5024242
Vedant <vedant10vaidya@gmail.com>
2025-03-22 22:25:20 +0900

    Spring ORM | Hibernate deep dive | For HQL & JPQL in Spring boot | Annotation practical & saving immage to db using hibernate

--- src/main/java/com/orm/hibernate/App.java
+++ src/main/java/com/orm/hibernate/App.java
@@ -1,10 +1,16 @@
 package com.orm.hibernate;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import com.orm.hibernate.entities.Address;
 import com.orm.hibernate.entities.Student;
 
 /**
@@ -13,7 +19,7 @@ import com.orm.hibernate.entities.Student;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Project started..." );
         
@@ -27,10 +33,24 @@ public class App
         
         System.out.println(student1);
         
+        
+        // Create new address
+        Address address1 = new Address();
+        address1.setStreet("8th street");
+        address1.setCity("Tokyo");
+        address1.setAddedDate(new Date());
+        address1.setOpen(true);
+        address1.setX(1234);
+        // Reading image
+		FileInputStream fis = new FileInputStream("src/main/java/image.jpg");
+		byte[] imageData = new byte[fis.available()];
+		address1.setImage(imageData);
+        
         Session session = sessionFactory.openSession();
         
         Transaction tx = session.beginTransaction();        
         session.persist(student1);
+        session.persist(address1);
         tx.commit();
         session.close();
     }
---
+++ src/main/java/com/orm/hibernate/entities/Address.java
@@ -0,0 +1,122 @@
+package com.orm.hibernate.entities;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "student_address")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "address_id")
+	private int id;
+	
+	@Column(length = 50)
+	private String street;
+	
+	@Column(length = 50)
+	private String city;
+	
+	@Column(name = "is_open")
+	private boolean isOpen;
+	
+	@Transient
+	private double x;
+	
+	@Column(name = "added_date")
+	@Temporal(TemporalType.DATE)
+	private Date addedDate;
+	
+	@Lob
+	private byte[] image;
+	
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address(int id, String street, String city, boolean isOpen, double x, Date addedDate, byte[] image) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.city = city;
+		this.isOpen = isOpen;
+		this.x = x;
+		this.addedDate = addedDate;
+		this.image = image;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public boolean isOpen() {
+		return isOpen;
+	}
+
+	public void setOpen(boolean isOpen) {
+		this.isOpen = isOpen;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	public byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street=" + street + ", city=" + city + ", isOpen=" + isOpen + ", x=" + x
+				+ ", addedDate=" + addedDate + ", image=" + Arrays.toString(image) + "]";
+	}	
+}
