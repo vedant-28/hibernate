d75e1c2563c78f707908a4564a5536daccc81362
Vedant <vedant10vaidya@gmail.com>
2025-03-30 17:05:21 +0900

    Spring ORM | Hibernate deep dive | Simple HQL queries

---
+++ src/main/java/com/orm/hql/HqlDemo.java
@@ -0,0 +1,58 @@
+package com.orm.hql;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.orm.hibernate.entities.Student;
+
+public class HqlDemo {
+ 
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		
+		// Simple HQL query:: To perform SELECT operation 
+		String query = "FROM Student";
+		Query<Student> q1 = session.createQuery(query, Student.class);
+		List<Student> students = q1.list();
+		for(Student s : students) {
+			System.out.println(s.getName());
+		}
+		
+		// HQL query with WHERE
+		String query2 = "FROM Student WHERE city='Tokyo'";
+		Query<Student> q2 = session.createQuery(query2, Student.class);
+		List<Student> list1 = q2.list();
+		for(Student s : list1) {
+			System.out.println(s.getName() + s.getCity());
+		}
+		
+		// Dynamic params in HQL query
+		String query3 = "FROM Student WHERE city=:value";
+		Query<Student> q3 = session.createQuery(query3, Student.class);
+		q3.setParameter("value", "Tokyo");
+		List<Student> list2 = q2.list();
+		for(Student s : list2) {
+			System.out.println(s.getName() + s.getCity());
+		}
+		
+		// HQL with table alias
+		String query4 = "FROM Student AS s WHERE s.city=:x AND s.name=:n";
+		Query<Student> q4 = session.createQuery(query4, Student.class);
+		q4.setParameter("value", "Tokyo");
+		q4.setParameter("value", "ABC");
+		List<Student> list3 = q4.list();
+		for(Student s : list3) {
+			System.out.println(s.getName() + s.getCity());
+		}
+		
+		session.close();
+	}
+	
+}
